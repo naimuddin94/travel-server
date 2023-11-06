5d9ad00165222f26592871e7ac4d48ccbbb99a5e
naimuddin94 <developernaimuddin@gmail.com>
2023-11-06 23:29:24 +0600

    initial setup with mongodb

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fv3bzks.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.log);
+
+app.get("/", async (req, res) => {
+  res.send("Welcome to the travlog server");
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
